f2f014b8b143f7d1522c65635f76f14047bb613c
Krishna.Pavuluri <Krishna.Pavuluri@Debenhamsgroup.com>
2026-06-11 14:37:21 +0100

    Test

--- JSON_Test.sql
+++ JSON_Test.sql
@@ -115,7 +115,7 @@ ORDER BY [Storage Type], OrderID;
 
 
 -- =====================================================================
--- CLEANUP Finally Test
+-- CLEANUP Finally Testes successfully
 -- =====================================================================
 -- DROP TABLE dbo.Orders_NVARCHAR;
 -- DROP TABLE dbo.Orders_JSON;
